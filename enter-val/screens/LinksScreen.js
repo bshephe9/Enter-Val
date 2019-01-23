f1a458fc0cd44339f37e54f8bd3a692f33e56f09
Veronica L <verolozada12@gmail.com>
2019-01-22 19:43:09 -0500

    example for form created

--- enter-val/screens/LinksScreen.js
+++ enter-val/screens/LinksScreen.js
@@ -1,24 +1,39 @@
 import React from 'react';
-import { ScrollView, StyleSheet, Text, Button } from 'react-native';
+import { ScrollView, StyleSheet, TextInput, Button, Form } from 'react-native';
 // import { ExpoLinksView } from '@expo/samples';
-import { FormLabel, FormInput, FormValidationMessage } from 'react-native-elements'
+import { FormLabel, FormInput, FormValidationMessage, Input } from 'react-native-elements'
 
 
 
 export default class LinksScreen extends React.Component {
+
+  state = {
+    name: ""
+  }
   static navigationOptions = {
     title: 'Links',
   };
-  hello(){
-    console.log("Hello");
+
+
+  submit = event => {
+    event.preventDefault();
+    console.log(`The title of the note is: ${this.state.name}`);
+    this.setState({ name: "" });
   }
+
+  change = event => {
+    console.log(event);
+    this.setState({name: event})
+  };
+
   render() {
     return (
       <ScrollView>
-      <FormLabel>Title</FormLabel>
-      <FormInput></FormInput>
-      <FormValidationMessage>{'Field required'}</FormValidationMessage>
-      <Button title="Submit" onPress={this.hello}/>
+        <FormLabel>Title</FormLabel>
+        <FormInput value={this.state.name} onChangeText={this.change}></FormInput>
+        {/* <Input value={this.state.name} onChangeText={name => this.updateValue({name})}/> */}
+        <FormValidationMessage>{'Field required'}</FormValidationMessage>
+        <Button title="Submit" onPress={this.submit} />
       </ScrollView>
     );
   }
