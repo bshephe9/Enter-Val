75e1a29443a0ace6bd24e5d03832764250f7c7ca
Bradley <brad.driver33@gmail.com>
2019-01-22 20:39:24 -0500

    k

---
+++ enter-val/screens/LoginScreen.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import { Alert, Button, TextInput, View, StyleSheet } from 'react-native';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      username: '',
+      password: '',
+    };
+  }
+  
+  onLogin() {
+    const { username, password } = this.state;
+
+    Alert.alert('Credentials', `${username} + ${password}`);
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <TextInput
+          value={this.state.username}
+          onChangeText={(username) => this.setState({ username })}
+          placeholder={'Username'}
+          style={styles.input}
+        />
+        <TextInput
+          value={this.state.password}
+          onChangeText={(password) => this.setState({ password })}
+          placeholder={'Password'}
+          secureTextEntry={true}
+          style={styles.input}
+        />
+        
+        <Button
+          title={'Login'}
+          style={styles.input}
+          onPress={this.onLogin.bind(this)}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+  },
+  input: {
+    width: 200,
+    height: 44,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: 'black',
+    marginBottom: 10,
+  },
+});
