eda11371de089d39aabf3855e2d7e8cd281149ee
Veronica L <verolozada12@gmail.com>
2019-01-26 12:11:46 -0500

    create task screen added

--- enter-val/screens/UserScreen.js
+++ enter-val/screens/UserScreen.js
@@ -13,70 +13,70 @@ export default class UserScreen extends React.Component {
 
       <ScrollView style={{ paddingVertical: 20 }}>
 
-        <Card title="8:00 - 11:00 AM">
+        <Card title='8:00 - 11:00 AM'>
           <View
             style={{
-              backgroundColor: "#bcbec1",
-              alignItems: "center",
-              justifyContent: "center",
+              backgroundColor: '#bcbec1',
+              alignItems: 'center',
+              justifyContent: 'center',
               width: 80,
               height: 80,
               borderRadius: 40,
-              alignSelf: "center",
+              alignSelf: 'center',
               marginBottom: 20
             }}
           >
-            <Text style={{ color: "white", fontSize: 28 }}>JD</Text>
+            <Text style={{ color: 'white', fontSize: 28 }}>JD</Text>
           </View>
         </Card>
-        <Card title="11:00 - 2:00 PM">
+        <Card title='11:00 - 2:00 PM'>
           <View
             style={{
-              backgroundColor: "#bcbec1",
-              alignItems: "center",
-              justifyContent: "center",
+              backgroundColor: '#bcbec1',
+              alignItems: 'center',
+              justifyContent: 'center',
               width: 80,
               height: 80,
               borderRadius: 40,
-              alignSelf: "center",
+              alignSelf: 'center',
               marginBottom: 20
             }}
           >
-            <Text style={{ color: "white", fontSize: 28 }}>JD</Text>
-          </View>
-        </Card>
-
-        <Card title="2:00 - 6:00 PM">
-          <View
-            style={{
-              backgroundColor: "#bcbec1",
-              alignItems: "center",
-              justifyContent: "center",
-              width: 80,
-              height: 80,
-              borderRadius: 40,
-              alignSelf: "center",
-              marginBottom: 20
-            }}
-          >
-            <Text style={{ color: "white", fontSize: 28 }}>JD</Text>
+            <Text style={{ color: 'white', fontSize: 28 }}>JD</Text>
           </View>
         </Card>
 
-        <Card title="6:00 - 10:00 PM">
+        <Card title='2:00 - 6:00 PM'>
           <View
             style={{
-              backgroundColor: "#bcbec1",
-              alignItems: "center",
-              justifyContent: "center",
+              backgroundColor: '#bcbec1',
+              alignItems: 'center',
+              justifyContent: 'center',
               width: 80,
               height: 80,
               borderRadius: 40,
-              alignSelf: "center",
+              alignSelf: 'center',
               marginBottom: 20
             }}
           >
-            <Text style={{ color: "white", fontSize: 28 }}>JD</Text>
+            <Text style={{ color: 'white', fontSize: 28 }}>JD</Text>
+          </View>
+        </Card>
+
+        <Card title='6:00 - 10:00 PM'>
+          <View
+            style={{
+              backgroundColor: '#bcbec1',
+              alignItems: 'center',
+              justifyContent: 'center',
+              width: 80,
+              height: 80,
+              borderRadius: 40,
+              alignSelf: 'center',
+              marginBottom: 20
+            }}
+          >
+            <Text style={{ color: 'white', fontSize: 28 }}>JD</Text>
           </View>
         </Card>
       </ScrollView>
