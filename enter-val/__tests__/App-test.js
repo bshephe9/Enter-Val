79e55675e03728d0c199f99d128526770b3cae34
Veronica L <verolozada12@gmail.com>
2019-02-07 18:08:37 -0500

    test added

--- enter-val/__tests__/App-test.js
+++ enter-val/__tests__/App-test.js
@@ -1,22 +1,94 @@
-import 'react-native';
 import React from 'react';
-import App from '../App';
 import renderer from 'react-test-renderer';
-import NavigationTestUtils from 'react-navigation/NavigationTestUtils';
+import App from '../App';
+import HomeScreen from '../screens/HomeScreen';
+import CreateTask from "../screens/CreateTask";
+import Login from "../screens/Login";
+import SignUp from "../screens/SignUp";
+import UserScreen from "../screens/UserScreen";
+//TODO check if we need this
+import SettingsScreen from "../screens/SettingsScreen";
 
-describe('App snapshot', () => {
-  jest.useFakeTimers();
-  beforeEach(() => {
-    NavigationTestUtils.resetInternalState();
-  });
+//* testing App
 
-  it('renders the loading screen', async () => {
-    const tree = renderer.create(<App />).toJSON();
-    expect(tree).toMatchSnapshot();
-  });
-
-  it('renders the root without loading screen', async () => {
-    const tree = renderer.create(<App skipLoadingScreen />).toJSON();
-    expect(tree).toMatchSnapshot();
-  });
+it('App renders without crashing', () => {
+const rendered = renderer.create(<App />).toJSON();
+expect(rendered).toBeTruthy();
 });
+
+it('App test against snapshot', () => {
+const tree = renderer.create(<App />).toJSON();
+expect(tree).toMatchSnapshot();
+});
+
+//* testing HomeScreen
+
+it('Home Screen renders without crashing', () => {
+    const rendered = renderer.create(<HomeScreen />).toJSON();
+    expect(rendered).toBeTruthy();
+    });
+    
+    it('Home Screen test against snapshot', () => {
+    const tree = renderer.create(<HomeScreen />).toJSON();
+    expect(tree).toMatchSnapshot();
+    });
+
+//* testing Login
+
+it('Login renders without crashing', () => {
+    const rendered = renderer.create(<Login />).toJSON();
+    expect(rendered).toBeTruthy();
+    });
+    
+    it('Login test against snapshot', () => {
+    const tree = renderer.create(<Login />).toJSON();
+    expect(tree).toMatchSnapshot();
+    });    
+
+//* testing Sign Up
+
+it('Sign up renders without crashing', () => {
+    const rendered = renderer.create(<SignUp />).toJSON();
+    expect(rendered).toBeTruthy();
+    });
+    
+    it('Sign up test against snapshot', () => {
+    const tree = renderer.create(<SignUp />).toJSON();
+    expect(tree).toMatchSnapshot();
+    });
+
+//* testing User Screen
+
+it('User Screen renders without crashing', () => {
+    const rendered = renderer.create(<UserScreen />).toJSON();
+    expect(rendered).toBeTruthy();
+    });
+    
+    it('User Screen test against snapshot', () => {
+    const tree = renderer.create(<UserScreen />).toJSON();
+    expect(tree).toMatchSnapshot();
+    });
+
+//* testing Tasks
+
+it('Tasks Page renders without crashing', () => {
+    const rendered = renderer.create(<CreateTask />).toJSON();
+    expect(rendered).toBeTruthy();
+    });
+    
+    it('Tasks Page test against snapshot', () => {
+    const tree = renderer.create(<CreateTask />).toJSON();
+    expect(tree).toMatchSnapshot();
+    });
+
+//TODO testing SettingsScreen?? 
+
+it('Settings renders without crashing', () => {
+    const rendered = renderer.create(<SettingsScreen />).toJSON();
+    expect(rendered).toBeTruthy();
+    });
+    
+    it('Settings test against snapshot', () => {
+    const tree = renderer.create(<SettingsScreen />).toJSON();
+    expect(tree).toMatchSnapshot();
+    });
